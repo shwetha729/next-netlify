a84684d80284ef31b19eed745459a4c6c55bdd46
Shwetha Jayaraj <shwetha.jayaraj@uconn.edu>
2023-06-19 19:45:03 -0400

    more

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Getting started <code>pages/index.js</code>
+          Getting started on <code>pages/index.js</code>
         </p>
       </main>
 
